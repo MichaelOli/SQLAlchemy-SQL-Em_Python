add2b48982462e2d9a21423a55e5f7aa1fecde28
Michael Oliveira Ribeiro <michaeloliveira38@gmail.com>
2025-06-11 10:21:26 -0300

    Entendendo classes e Sessão com SQLAlchemy

---
+++ exemplo_01.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+engine = create_engine("sqlite:///meubanco.db", echo=True)
+
+
+Base = declarative_base()
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+# Criar as tabelas no banco de dados
+Base.metadata.create_all(engine)        
+# Criar um usuário
+novo_usuario = Usuario(nome='João', idade=30)
+# Adicionar o usuário à sessão
+session.add(novo_usuario)
+# Salvar as alterações no banco de dados        
+session.commit()
+# Consultar todos os usuários           
+usuarios = session.query(Usuario).all()
+for usuario in usuarios:
+    print(f'ID: {usuario.id}, Nome: {usuario.nome}, Idade: {usuario.idade}')
+# Fechar a sessão
+session.close()
+# Exemplo de uso do SQLAlchemy com SQLite
+# Este exemplo cria um banco de dados SQLite, define uma tabela de usuários,    
+# adiciona um usuário e consulta todos os usuários existentes.
+# Certifique-se de ter o SQLAlchemy instalado: pip install sqlalchemy
+
+# Para executar este código, certifique-se de ter o SQLAlchemy instalado:
+# pip install sqlalchemy
+# O banco de dados será criado no mesmo diretório do script com o nome "meubanco.db"
